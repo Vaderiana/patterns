48d86f16e7dfe178c34828ce0e9544061f6f260b
Vaderiana <ermolina90@gmail.com>
2024-03-17 13:49:00 +0400

    fix first task

--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -15,7 +15,7 @@ class DeliveryTest {
 
     @BeforeEach
     void setup() {
-       open("http://localhost:9999");
+        open("http://localhost:9999");
     }
 
     @Test
@@ -34,15 +34,23 @@ class DeliveryTest {
         $(By.cssSelector("[name='phone']")).sendKeys(validUser.getPhone());
         $(By.cssSelector("[class='checkbox__text']")).click();
         $(By.cssSelector("button.button")).click();
-        $(By.cssSelector("button.button")).click();
         String text = $("[class='notification__title']").getText();
-        if (text.equals("Необходимо подтверждение")){
-            $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-            $("[data-test-id='date'] input").setValue(secondMeetingDate);
-            $(By.cssSelector("button.button")).click();
-            text = $("[class='notification__title']").getText();
-            actualDate = secondMeetingDate;
-        }
+        assertEquals("Успешно!", text);
+        $("[data-test-id='success-notification'] [class='notification__content']")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + actualDate))
+                .shouldBe(Condition.visible);
+        $(By.cssSelector("button.button")).click();
+
+        $("[data-test-id='replan-notification'] [class='notification__content']")
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(Condition.visible);
+        $(By.cssSelector("button.button")).click();
+
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(secondMeetingDate);
+        $(By.cssSelector("button.button")).click();
+        text = $("[class='notification__title']").getText();
+        actualDate = secondMeetingDate;
         assertEquals("Успешно!", text);
         $("[data-test-id='success-notification'] [class='notification__content']")
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + actualDate))
